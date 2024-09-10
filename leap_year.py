e34c5b5883ec71c8a5fb3b60fc328103c0333a9b
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-09-10 16:49:27 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,12 @@
 def leap_year():
-    year=int(input("Ingrese un year:"))
-if (year % 400 == 0) or (year % 4 == 0 and year % 100):
-    print(f"El año {year} es bisiesto")
-else:
-    print(f"El año {year} no es bisiesto")
+    y = int(input("Ingrese un año: "))
+
+    d4 = not y % 4      # Es divisible por 4?
+    d100 = not y % 100  # Es divisible por 100?
+    d400 = not y % 400  # Es divisible por 400?
+
+    # Si es div. por 4 pero no centenario ó div. por 400
+    if (d4 and not d100) or d400:
+        print(f"El año {y} es bisiesto")
+    else:
+        print(f"El año {y} no es bisiesto")
